c997d93708d4857fc3226ee24c3e64e5aeb6dbd2
Rubèn <numbworks@gmail.com>
2024-02-09 14:03:34 +0100

    #1 NW.Shared.Files: *.cs added.

---
+++ src/NW.Shared.Files/IFileManager.cs
@@ -0,0 +1,48 @@
+﻿namespace NW.Shared.Files
+{
+    /// <summary>Collects all the helper methods related to file management.</summary>
+    public interface IFileManager
+    {
+
+        /// <summary>
+        /// Opens a text file, reads all lines of the file, and then closes the file.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        IEnumerable<string> ReadAllLines(IFileInfoAdapter file);
+
+        /// <summary>
+        /// Opens a text file, reads all lines of the file, and then closes the file.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        string ReadAllText(IFileInfoAdapter file);
+
+        /// <summary>
+        /// Creates a new file, writes a collection of strings to the file, and then closes the file.
+        /// </summary>
+        /// <exception cref="Exception"></exception> 
+        void WriteAllLines(IFileInfoAdapter file, IEnumerable<string> content);
+
+        /// <summary>
+        /// Creates a new file, writes the specified string to the file, and then closes
+        /// the file. If the target file already exists, it is overwritten.
+        /// </summary>
+        /// <exception cref="Exception"></exception>  
+        void WriteAllText(IFileInfoAdapter file, string content);
+
+        /// <summary>
+        /// Initializes a <see cref="IFileInfoAdapter"/> instance for <paramref name="filePath"/>.
+        /// </summary>    
+        IFileInfoAdapter Create(string filePath);
+
+        /// <summary>
+        /// Initializes a <see cref="IFileInfoAdapter"/> instance for <paramref name="fileInfo"/>.
+        /// </summary>        
+        IFileInfoAdapter Create(FileInfo fileInfo);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 09.02.2024
+*/
