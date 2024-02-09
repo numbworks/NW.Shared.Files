7208a19bd1554e07d135914654c08ed455e39035
Rubèn <numbworks@gmail.com>
2024-02-09 14:36:52 +0100

    #1 NW.Shared.Files.UnitTests: *.cs added.

---
+++ tests/NW.Shared.Files.UnitTests/ObjectMother.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using NW.Shared.Files.UnitTests.Utilities;
+
+namespace NW.Shared.Files.UnitTests
+{
+    public static class ObjectMother
+    {
+
+        #region Properties
+
+        public static string ContentSingleLine = "First line";
+        public static IEnumerable<string> ContentMultipleLines =
+            new List<string>() {
+                "First line",
+                "Second line"
+            };
+        public static string FileInfoAdapterFullName = @"C:\somefile.txt";
+        public static IFileInfoAdapter FileInfoAdapterDoesntExist
+            => new FakeFileInfoAdapter(false, FileInfoAdapterFullName);
+        public static IFileInfoAdapter FileInfoAdapterExists
+            => new FakeFileInfoAdapter(true, FileInfoAdapterFullName);
+        public static IOException FileAdapterIOException = new IOException("Impossible to access the file.");
+        public static IFileAdapter FileAdapterReadAllMethodsThrowIOException
+            => new FakeFileAdapter(
+                    fakeReadAllLines: () => throw FileAdapterIOException,
+                    fakeReadAllText: () => throw FileAdapterIOException
+                );
+        public static IFileAdapter FileAdapterWriteAllMethodsThrowIOException
+            => new FakeFileAdapter(
+                    fakeWriteAllLines: () => throw FileAdapterIOException,
+                    fakeWriteAllText: () => throw FileAdapterIOException
+                );
+        public static IFileAdapter FileAdapterAllMethodsWork
+            => new FakeFileAdapter(
+                    fakeReadAllLines: () => ContentMultipleLines.ToArray(),
+                    fakeReadAllText: () => ContentSingleLine,
+                    fakeWriteAllLines: () => { },
+                    fakeWriteAllText: () => { }
+                );
+
+        #endregion
+
+        #region Methods
+
+        public static void Method_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+        {
+
+            // Arrange
+            // Act
+            // Assert
+            Exception actual = Assert.Throws(expectedType, del);
+            Assert.That(actual.Message, Is.EqualTo(expectedMessage));
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 09.02.2024
+*/
