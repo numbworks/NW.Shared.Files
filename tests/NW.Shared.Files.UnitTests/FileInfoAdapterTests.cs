e41c94d3cb2197f6150edd4f46c9667adb423b08
Rubèn <numbworks@gmail.com>
2024-02-11 18:06:44 +0100

    #1 FileInfoAdapterTests: added.

---
+++ tests/NW.Shared.Files.UnitTests/FileInfoAdapterTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NW.Shared.Files.UnitTests
+{
+    [TestFixture]
+    public class FileInfoAdapterTests
+    {
+
+        #region Fields
+
+        private static TestCaseData[] fileInfoAdapterExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate( () => new FileInfoAdapter(fileInfo: null) ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("fileInfo").Message
+                ).SetArgDisplayNames($"{nameof(fileInfoAdapterExceptionTestCases)}_01"),
+
+        };
+
+        #endregion
+
+        #region Tests
+
+        [TestCaseSource(nameof(fileInfoAdapterExceptionTestCases))]
+        public void FileInfoAdapter_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 11.02.2024
+*/
